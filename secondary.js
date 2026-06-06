a3fe93539a7e5777b157a48cfda5a23e8515e973
DarioM1992 <dario21992@gmail.com>
2026-06-06 13:27:44 +0200

    creato array squadre calcio

---
+++ secondary.js
@@ -0,0 +1,34 @@
+const squadre =  [
+    { nome: "Inter",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Roma",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Juve",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Sassuolo",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Milan",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Salernitana",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Como",
+      punti: 0,
+      falliSubiti: 0
+    },
+    { nome: "Napoli",
+      punti: 0,
+      falliSubiti: 0
+    },        
+];
